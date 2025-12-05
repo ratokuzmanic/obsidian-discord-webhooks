77ba25c5f519cd4ceb02dfbfd6cdbf6fb26559e6
Rato Kuzmanić <rato.kuzmanic@live.com>
2025-12-05 15:25:06 +0100

    Add registering command for selected text webhook

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Plugin } from 'obsidian';
 import { DiscordWebhooksSettings } from './types';
 import DiscordWebhooksSettingTab from './DiscordWebhooksSettingTab';
+import Commands from './Commands';
 
 const DEFAULT_SETTINGS: DiscordWebhooksSettings = {
   webhooks: [],
@@ -10,13 +11,23 @@ const DEFAULT_SETTINGS: DiscordWebhooksSettings = {
 
 export default class DiscordWebhooksPlugin extends Plugin {
   settings: DiscordWebhooksSettings;
+  commands: Commands;
 
   async onload() {
     this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
     this.addSettingTab(new DiscordWebhooksSettingTab(this.app, this));
+
+    this.commands = new Commands(this);
+    this.commands.enable();
+    this.commands.registerCommands();
+  }
+
+  onunload() {
+    this.commands.disable();
   }
 
   async saveSettings() {
     await this.saveData(this.settings);
+    this.commands.registerCommands();
   }
 }
